722dfb60c2c4e8d1cce08008314fcfae1408eee0
Nikhil Suwalka <suwalka18@gmail.com>
2021-10-16 12:33:04 +0530

    Made some user form fields disabled.
    Signed-off-by: Nikhil Suwalka <suwalka18@gmail.com>

--- Klubworks/forms.py
+++ Klubworks/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import *
 
+
 class ProfileForm(forms.ModelForm):
+    first_name = forms.CharField(disabled=True)
+    last_name = forms.CharField(disabled=True)
+    email = forms.CharField(disabled=True)
+    address = forms.CharField(required=False)
+    user_type = forms.IntegerField(disabled=True)
+
     class Meta:
         model = User
-        fields = ['first_name','last_name','email','uid','college_name','address','phone_no','user_type']
+        fields = ['first_name', 'last_name', 'email', 'uid', 'college_name', 'address', 'phone_no', 'user_type']
