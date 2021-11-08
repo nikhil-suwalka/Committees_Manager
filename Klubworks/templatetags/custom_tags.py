42c8d0968f4cc871accbf6c5253815530d84d4ec
nishit <nishit.shanbhag@spit.ac.in>
2021-11-08 13:52:41 +0530

    changed banner(GUI)

--- Klubworks/templatetags/custom_tags.py
+++ Klubworks/templatetags/custom_tags.py
@@ -11,3 +11,15 @@ def get_item(dictionary, key):
 @register.filter
 def get_item_from_model_obj(obj, col):
     return getattr(obj, col)
+
+
+@register.filter
+def get_image_name_from_path(path):
+    return str(path).split("/")[-1]
+
+
+@register.filter
+def ellipsis(para):
+    if len(str(para)) > 125:
+        return para[:125] + "..."
+    return para
